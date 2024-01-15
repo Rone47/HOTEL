b8c51258791f0befa60d16f9baa00f02f0a5ae9c
Rone47 <ryanbyrone47@gmail.com>
2024-01-15 21:03:46 +0300

    Authentication page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,6 +3,7 @@ import { FcGoogle } from "react-icons/fc"
 
 
 const Auth = () => {
+    const inputStyles = 'border border-gray-200 sm:text-sm text:black rounded-lg block w-full p-2.5 focus:outline-none border-b-4 ';
   return (
     <section className="container mx-auto">
         <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
@@ -10,14 +11,43 @@ const Auth = () => {
                 <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl">
                     Create an account
                 </h1>
-                <p>
+                <p className="font-semibold">
                     OR
                 </p>
                 <span className="inline-flex items-center">
-                    <AiFillFacebook className="mr-3 text-4xl cursor-pointer text-sky-700 dark:text-white"/>
+                    <AiFillFacebook className="mr-3 text-4xl cursor-pointer text-sky-700 dark:text-white"/> 
+                    |
                     <FcGoogle className="ml-3 text-4xl cursor-pointer"/>
                 </span>
             </div>
+            <form className="space-y-4 md:space-y-6">
+                <input 
+                 type="email" 
+                 name="email"
+                 id="eamil"
+                 placeholder="example@gmail.com"
+                 required
+                 className={inputStyles}
+                 />
+                      <input 
+                 type="text" 
+                 name="name"
+                 id="name"
+                 placeholder="James Smith"
+                 required
+                 className={inputStyles}
+                 />
+                 <input 
+                 type="password" 
+                 name="password"
+                 id="password"
+                 minLength={8}
+                 placeholder="password"
+                 required
+                 className={inputStyles}
+                 />
+             
+            </form>
         </div>
     </section> 
   )
