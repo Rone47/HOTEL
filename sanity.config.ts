73cd881fc036bbb3b6fdded622ed34dd4944945a
Rone47 <ryanbyrone47@gmail.com>
2024-01-17 12:24:35 +0300

    Completed Schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'Hotel-Management',
 
-  projectId: 'wyk5uk62',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
   
   basePath: '/studio',
 
