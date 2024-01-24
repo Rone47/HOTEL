29eefa3be4b178abfed8deab1c95786393d4fb30
Rone47 <ryanbyrone47@gmail.com>
2024-01-24 11:03:39 +0300

    updates

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <section className=''>Search</section>
+    <section className='bg-tertiary-light px-4 py-6 rounded-lg'>
+      <div className='container mx-auto flex gap-4 flex-wrap justify-between items-center'>
+        <div className='w-full md:1/3 lg:w-auto mb-4 md:mb-0'></div>
+      </div>
+    </section>
   )
 }
 
