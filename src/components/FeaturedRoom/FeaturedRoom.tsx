da07cd052003d943693cfc7b3b1d7ffddb0d21ed
Rone47 <ryanbyrone47@gmail.com>
2024-01-25 15:43:32 +0300

    updates

--- src/components/FeaturedRoom/FeaturedRoom.tsx
+++ src/components/FeaturedRoom/FeaturedRoom.tsx
@@ -63,7 +63,7 @@ const FeaturedRoom: FC<Props> = props => {
 
           <Link
             href={`/rooms/${featuredRoom.slug.current}`}
-            className='border h-fit text-center border-cyan-500 text-cyan-500 px-3 py-2 lg:py-5 lg:px-7 rounded-2xl font-bold lg:text-xl'
+            className='border h-fit text-center border-cyan-500 text-cyan-500 px-3 py-2 lg:py-5 lg:px-7 rounded-3xl font-bold lg:text-xl'
           >
             More Details
           </Link>
