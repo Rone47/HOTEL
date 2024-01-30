0ec54030098971cff78897a01332caa637c46db2
Rone47 <ryanbyrone47@gmail.com>
2024-01-30 20:23:07 +0300

    updates

---
+++ src/app/(web)/rooms/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const Rooms = () => {
+    const [roomTypeFilter, setRoomTypeFilter] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const searchQuery = searchParams.get('searchQuery');
+        const roomType = searchParams.get('roomType');
+
+        if (roomType) setRoomTypeFilter(roomType);
+        if (searchQuery) setSearchQuery(searchQuery);
+      
+    }, [searchParams]);
+
+
+  return (
+    <div>Rooms</div>
+  )
+}
+
+export default Rooms;
