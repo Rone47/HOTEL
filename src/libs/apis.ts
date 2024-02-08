142474b13ff1f02f700e2198310bfb00bdd6e636
Rone47 <ryanbyrone47@gmail.com>
2024-02-08 17:07:33 +0300

    updates

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -3,15 +3,30 @@ import sanityClient from "./sanity";
 import * as queries from './sanityQueries';
 
 export async function getFeaturedRoom() {
-    const result = await sanityClient.fetch <Room>(
-        queries.getFeaturedRoomQuery,
-         {},
-         {cache: 'no-cache'}
-          );
-          return result;
-}
-
-export async function getRooms() {
-    const result = await sanityClient.fetch<Room[]>(queries.getRoomsQuery);
+    const result = await sanityClient.fetch<Room>(
+      queries.getFeaturedRoomQuery,
+      {},
+      { cache: 'no-cache' }
+    );
+  
     return result;
-}
+  }
+  
+  export async function getRooms() {
+    const result = await sanityClient.fetch<Room[]>(
+      queries.getRoomsQuery,
+      {},
+      { cache: 'no-cache' }
+    );
+    return result;
+  }
+
+export async function getRoom(slug: string) {
+    const result = await sanityClient.fetch<Room>(
+      queries.getRoom,
+      { slug },
+      { cache: 'no-cache' }
+    );
+  
+    return result;
+  }
