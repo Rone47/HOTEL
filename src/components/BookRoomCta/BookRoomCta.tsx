7f50050ae83a35b813ec612cc69e0171b4adc24c
Rone47 <ryanbyrone47@gmail.com>
2024-02-09 10:21:23 +0300

    updates

---
+++ src/components/BookRoomCta/BookRoomCta.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookRoomCta = () => {
+  return (
+    <div>BookRoomCta</div>
+  )
+}
+
+export default BookRoomCta
---
+++ src/components/RoomReview/RoomReview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoomReview = () => {
+  return (
+    <div>RoomReview</div>
+  )
+}
+
+export default RoomReview
