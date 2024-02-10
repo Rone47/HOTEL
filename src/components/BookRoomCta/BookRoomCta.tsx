5c934c8f0f2b6a461d165bff10bc2b3de2f6f913
Rone47 <ryanbyrone47@gmail.com>
2024-02-10 13:25:55 +0300

    updates

--- src/components/BookRoomCta/BookRoomCta.tsx
+++ src/components/BookRoomCta/BookRoomCta.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+'use client';
 
-const BookRoomCta = () => {
+import {FC} from 'react';
+
+type Props = {
+  price: number;
+  discount: number;
+};
+
+const BookRoomCta: FC<Props> = props => {
+  const { price, discount } = props;
+  const discountPrice = price - (price / 100) * discount;
   return (
-    <div>BookRoomCta</div>
+    <div className='px-7 py-6'>
+      <h3>
+        <span className={`${discount ? 'text-gray-600' : ''} font-bold text-xl`}>
+          ${price}
+        </span>
+        {discount ? (
+          <span className='font-bold text-xl'>
+            | discount {discount}%. NOW {''}
+            <span className='text-blue-500'>$ {discountPrice}</span>
+          </span>
+        ) : (
+          ''
+        )}
+      </h3>
+    </div>
   )
 }
 
