c6804f6afb8378b9cfad0153475f6351081aa5dd
Rone47 <ryanbyrone47@gmail.com>
2024-02-10 14:02:47 +0300

    update

--- src/components/BookRoomCta/BookRoomCta.tsx
+++ src/components/BookRoomCta/BookRoomCta.tsx
@@ -5,19 +5,20 @@ import {FC} from 'react';
 type Props = {
   price: number;
   discount: number;
+  specialNote: string;
 };
 
 const BookRoomCta: FC<Props> = props => {
-  const { price, discount } = props;
+  const { price, discount, specialNote } = props;
   const discountPrice = price - (price / 100) * discount;
   return (
     <div className='px-7 py-6'>
       <h3>
-        <span className={`${discount ? 'text-gray-600' : ''} font-bold text-xl`}>
+        <span className={`${discount ? 'text-gray-500' : ''} font-bold text-xl`}>
           ${price}
         </span>
         {discount ? (
-          <span className='font-bold text-xl'>
+          <span className='font-bold text-xl'> {''}
             | discount {discount}%. NOW {''}
             <span className='text-blue-500'>$ {discountPrice}</span>
           </span>
@@ -25,6 +26,9 @@ const BookRoomCta: FC<Props> = props => {
           ''
         )}
       </h3>
+      <div className='w-full border-b-2 border-blue-300 my-2' />
+
+      <h4 className='my-8'>{specialNote}</h4>
     </div>
   )
 }
