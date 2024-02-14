28e0bcf3cafc79d026064029005ccaf81b86cecd
Rone47 <ryanbyrone47@gmail.com>
2024-02-14 22:22:09 +0300

    update

--- src/components/BookRoomCta/BookRoomCta.tsx
+++ src/components/BookRoomCta/BookRoomCta.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import {Dispatch, FC, SetStateAction} from 'react';
+import { Dispatch, FC, SetStateAction } from 'react';
 import DatePicker from 'react-datepicker';
-import  'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker.css';
 
 type Props = {
   checkinDate: Date | null;
@@ -10,57 +10,76 @@ type Props = {
   checkoutDate: Date | null;
   setCheckoutDate: Dispatch<SetStateAction<Date | null>>;
   setAdults: Dispatch<SetStateAction<number>>;
-  setChildren: Dispatch<SetStateAction<number>>;
+  setNoOfChildren: Dispatch<SetStateAction<number>>;
   calcMinCheckoutDate: () => Date | null;
   price: number;
   discount: number;
   adults: number;
-  children: number;
+  noOfChildren: number;
   specialNote: string;
+  isBooked: boolean;
+  handleBookNowClick: () => void;
 };
 
 const BookRoomCta: FC<Props> = props => {
-  const { 
-    price, 
-    discount, 
-    specialNote, 
-    checkinDate, 
-    setCheckinDate, 
-    checkoutDate, 
-    setCheckoutDate, 
+  const {
+    price,
+    discount,
+    specialNote,
+    checkinDate,
+    setCheckinDate,
+    checkoutDate,
+    setCheckoutDate,
     calcMinCheckoutDate,
     setAdults,
-    setChildren,
+    setNoOfChildren,
     adults,
-    children,
+    noOfChildren,
+    isBooked,
+    handleBookNowClick,
   } = props;
 
   const discountPrice = price - (price / 100) * discount;
+
+  const calcNoOfDays = () => {
+    if (!checkinDate || !checkoutDate) return 0;
+    const timeDiff = checkoutDate.getTime() - checkinDate.getTime();
+    const noOfDays = Math.ceil(timeDiff / (24 * 60 * 60 * 1000));
+    return noOfDays;
+  };
+
   return (
     <div className='px-7 py-6'>
       <h3>
-        <span className={`${discount ? 'text-gray-500' : ''} font-bold text-xl`}>
-          ${price}
+        <span
+          className={`${discount ? 'text-gray-400' : ''} font-bold text-xl`}
+        >
+          $ {price}
         </span>
         {discount ? (
-          <span className='font-bold text-xl'> {''}
-            | discount {discount}%. NOW {''}
-            <span className='text-blue-500'>$ {discountPrice}</span>
+          <span className='font-bold text-xl'>
+            {' '}
+            | discount {discount}%. Now{' '}
+            <span className='text-sky-500'>$ {discountPrice}</span>
           </span>
         ) : (
           ''
         )}
       </h3>
-      <div className='w-full border-b-2 border-blue-300 my-2' />
+
+      <div className='w-full border-b-2 border-cyan-500 my-2' />
 
       <h4 className='my-8'>{specialNote}</h4>
 
       <div className='flex'>
         <div className='w-1/2 pr-2'>
-          <label htmlFor="check-in-date" className='block text-sm font-medium text-gray-900 dark:text-gray-400'>
-            Check in date
+          <label
+            htmlFor='check-in-date'
+            className='block text-sm font-medium text-gray-900 dark:text-gray-400'
+          >
+            Check In date
           </label>
-          <DatePicker 
+          <DatePicker
             selected={checkinDate}
             onChange={date => setCheckinDate(date)}
             dateFormat='dd/MM/yyyy'
@@ -69,13 +88,12 @@ const BookRoomCta: FC<Props> = props => {
             className='w-full border text-black border-gray-300 rounded-lg p-2.5 focus:ring-primary focus:border-primary'
           />
         </div>
-
         <div className='w-1/2 pl-2'>
           <label
             htmlFor='check-out-date'
             className='block text-sm font-medium text-gray-900 dark:text-gray-400'
           >
-            Check out date
+            Check Out date
           </label>
           <DatePicker
             selected={checkoutDate}
@@ -87,26 +105,60 @@ const BookRoomCta: FC<Props> = props => {
             className='w-full border text-black border-gray-300 rounded-lg p-2.5 focus:ring-primary focus:border-primary'
           />
         </div>
+      </div>
 
-        <div className='flex mt-4'>
-          <div className='w-1/2 pr-2'>
-            <label htmlFor="adults" className='block text-sm font font-medium text-gray-900 dark:text-gray-400'>
-              Adults
-            </label>
-            <input 
-              type="number" 
-              id='adults' 
-              value={adults} 
-              onChange={e => setAdults(+e.target.value)}
-              min={1}
-              max={5}
-              className='w-full border border-gray-300 rounded-lg p-2.5'
-            />
-          </div>
+      <div className='flex mt-4'>
+        <div className='w-1/2 pr-2'>
+          <label
+            htmlFor='adults'
+            className='block text-sm font-medium text-gray-900 dark:text-gray-400'
+          >
+            Adults
+          </label>
+          <input
+            type='number'
+            id='adults'
+            value={adults}
+            onChange={e => setAdults(+e.target.value)}
+            min={1}
+            max={5}
+            className='w-full border border-gray-300 rounded-lg p-2.5'
+          />
+        </div>
+        <div className='w-1/2 pl-2'>
+          <label
+            htmlFor='children'
+            className='block text-sm font-medium text-gray-900 dark:text-gray-400'
+          >
+            Children
+          </label>
+          <input
+            type='number'
+            id='children'
+            value={noOfChildren}
+            onChange={e => setNoOfChildren(+e.target.value)}
+            min={0}
+            max={5}
+            className='w-full border border-gray-300 rounded-lg p-2.5'
+          />
         </div>
       </div>
-    </div>
-  )
-}
 
-export default BookRoomCta
+      {calcNoOfDays() > 0 ? (
+        <p className='mt-3 font-bold'>Total Price: $ {calcNoOfDays() * discountPrice}</p>
+      ) : (
+        <></>
+      )}
+
+      <button
+        disabled={isBooked}
+        onClick={handleBookNowClick}
+        className='btn-primary w-full mt-6 disabled:bg-gray-500 disabled:cursor-not-allowed'
+      >
+        {isBooked ? 'Booked' : 'Book Now'}
+      </button>
+    </div>
+  );
+};
+
+export default BookRoomCta;
