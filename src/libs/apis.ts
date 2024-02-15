0a646ca47d3fb544ba91f87f3426e207ae34c1a2
Rone47 <ryanbyrone47@gmail.com>
2024-02-15 11:38:07 +0300

    updates

--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { CreateBookingDto, Room } from "@/models/room";
 import sanityClient from "./sanity";
 import * as queries from './sanityQueries';
@@ -60,5 +61,13 @@ export const createBooking = async ({
       },
     ],
   };
+
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
+  );
+
+  return data;
 
 };
