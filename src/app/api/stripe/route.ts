354a3dca70757b3516c82991964997cf32eac174
Rone47 <ryanbyrone47@gmail.com>
2024-02-15 12:57:23 +0300

    update

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,38 +1,97 @@
+import Stripe from 'stripe';
+
+import { authOptions } from '@/libs/auth';
+import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
-import  Stripe from 'stripe'
+import { getRoom } from '@/libs/apis';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-    apiVersion: '2023-10-16',
+  apiVersion: '2023-10-16',
 });
 
 type RequestData = {
-    checkinDate: string;
-    checkoutDate: string;
-    adults: number;
-    children: number;
-    numberOfDays: number;
-    hotelRoomSlug: string;
-  };
+  checkinDate: string;
+  checkoutDate: string;
+  adults: number;
+  children: number;
+  numberOfDays: number;
+  hotelRoomSlug: string;
+};
 
 export async function POST(req: Request, res: Response) {
-    const {
-        checkinDate,
+  const {
+    checkinDate,
+    adults,
+    checkoutDate,
+    children,
+    hotelRoomSlug,
+    numberOfDays,
+  }: RequestData = await req.json();
+
+  if (
+    !checkinDate ||
+    !checkoutDate ||
+    !adults ||
+    !hotelRoomSlug ||
+    !numberOfDays
+  ) {
+    return new NextResponse('Please all fields are required', { status: 400 });
+  }
+
+  const origin = req.headers.get('origin');
+
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new NextResponse('Authentication required', { status: 400 });
+  }
+
+  const userId = session.user.id;
+  const formattedCheckoutDate = checkoutDate.split('T')[0];
+  const formattedCheckinDate = checkinDate.split('T')[0];
+
+  try {
+    const room = await getRoom(hotelRoomSlug);
+    const discountPrice = room.price - (room.price / 100) * room.discount;
+    const totalPrice = discountPrice * numberOfDays;
+
+    
+    const stripeSession = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: room.name,
+              images: room.images.map(image => image.url),
+            },
+            unit_amount: parseInt((totalPrice * 100).toString()),
+          },
+        },
+      ],
+      payment_method_types: ['card'],
+      success_url: `${origin}/users/${userId}`,
+      metadata: {
         adults,
-        checkoutDate,
+        checkinDate: formattedCheckinDate,
+        checkoutDate: formattedCheckoutDate,
         children,
-        hotelRoomSlug,
+        hotelRoom: room._id,
         numberOfDays,
-      }: RequestData = await req.json();
-
-      if (
-        !checkinDate ||
-        !checkoutDate ||
-        !adults ||
-        !hotelRoomSlug ||
-        !numberOfDays
-      ) {
-        return new NextResponse('Please all fields are required', { status: 400 });
+        user: userId,
+        discount: room.discount,
+        totalPrice
       }
+    });
 
-      const origin = req.headers.get('origin');
-};
+    return NextResponse.json(stripeSession, {
+      status: 200,
+      statusText: 'Payment session created',
+    });
+  } catch (error: any) {
+    console.log('Payment falied', error);
+    return new NextResponse(error, { status: 500 });
+  }
+}
