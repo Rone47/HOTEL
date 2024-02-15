1588ee241c98acae66ae8c6fa2c22674b8b0ef64
Rone47 <ryanbyrone47@gmail.com>
2024-02-15 21:12:03 +0300

    updates

---
+++ src/app/api/webhook/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+const checkout_session_completed = 'checkout.session.completed';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: '2023-10-16',
+  });
+
+
+  export async function POST(req: Request, res: Response) {
+    const reqBody = await req.text();
+    const sig = req.headers.get('stripe-signature');
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
+
+    let event: Stripe.Event;
+
+    try {
+
+      if (!sig || !webhookSecret) return;
+      event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+    } catch (error: any) {
+      return new NextResponse(`Webhook Error: ${error.message}`, { status: 500});
+    }
+
+    //load event
+    switch (event.type) {
+      case checkout_session_completed:
+        const session = event.data.object;
+        console.log(session);
+
+        //Create a Booking
+        return NextResponse.json('Booking Successful', {
+          status: 200,
+          statusText: 'Booking Successful',
+        });
+        
+        default: 
+        console.log(`Unhandled event type: ${event.type}`);
+    }
+  }
