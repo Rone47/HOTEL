72a4cf727e0be3509246a240be520505fadd6f73
Rone47 <ryanbyrone47@gmail.com>
2024-02-15 12:29:23 +0300

    Addd

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+import  Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: '2023-10-16',
+});
+
+type RequestData = {
+    checkinDate: string;
+    checkoutDate: string;
+    adults: number;
+    children: number;
+    numberOfDays: number;
+    hotelRoomSlug: string;
+  };
+
+export async function POST(req: Request, res: Response) {
+    const {
+        checkinDate,
+        adults,
+        checkoutDate,
+        children,
+        hotelRoomSlug,
+        numberOfDays,
+      }: RequestData = await req.json();
+
+      if (
+        !checkinDate ||
+        !checkoutDate ||
+        !adults ||
+        !hotelRoomSlug ||
+        !numberOfDays
+      ) {
+        return new NextResponse('Please all fields are required', { status: 400 });
+      }
+
+      const origin = req.headers.get('origin');
+};
