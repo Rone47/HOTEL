f885d4cf21af44f1bfd4ddd8d2715c25541c22b0
Rone47 <ryanbyrone47@gmail.com>
2024-02-19 19:51:12 +0300

    add

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,9 +3,10 @@ import { NextResponse } from 'next/server';
 
 import { authOptions } from '@/libs/auth';
 import {
-  
+  checkReviewExists,
+  createReview,
   getUserData,
-  
+  updateReview,
 } from '@/libs/apis';
 
 export async function GET(req: Request, res: Response) {
@@ -25,3 +26,44 @@ export async function GET(req: Request, res: Response) {
   }
 }
 
+export async function POST(req: Request, res: Response) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new NextResponse('Authentication Required', { status: 500 });
+  }
+
+  const { roomId, reviewText, ratingValue } = await req.json();
+
+  if (!roomId || !reviewText || !ratingValue) {
+    return new NextResponse('All fields are required', { status: 400 });
+  }
+
+  const userId = session.user.id;
+
+  try {
+    const alreadyExists = await checkReviewExists(userId, roomId);
+
+    let data;
+
+    if (alreadyExists) {
+      data = await updateReview({
+        reviewId: alreadyExists._id,
+        reviewText,
+        userRating: ratingValue,
+      });
+    } else {
+      data = await createReview({
+        hotelRoomId: roomId,
+        reviewText,
+        userId,
+        userRating: ratingValue,
+      });
+    }
+
+    return NextResponse.json(data, { status: 200, statusText: 'Successful' });
+  } catch (error: any) {
+    console.log('Error Updating', error);
+    return new NextResponse('Unable to create review', { status: 400 });
+  }
+}
