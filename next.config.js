c3d9af4c3430a649a561114bb3e8bb043b091454
Rone47 <ryanbyrone47@gmail.com>
2024-02-21 10:46:14 +0300

    updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [ "lh3.googleusercontent.com","avatars.githubusercontent.com", 'images.unsplash.com'],
+ images: {
+   domains: [ "lh3.googleusercontent.com","avatars.githubusercontent.com", 'images.unsplash.com'],
   },
 };
 
