ceaa913e8d0ec97618651903275e158a6875f6d5
Rone47 <ryanbyrone47@gmail.com>
2024-02-21 21:17:57 +0300

    upddates

--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'next-sanity';
+import { createClient } from '@sanity/client';
 
 const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
