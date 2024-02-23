7762db6eaf6ac9924449a9bb194db417c2838f2f
Rone47 <ryanbyrone47@gmail.com>
2024-02-23 19:41:09 +0300

    updates

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -5,9 +5,9 @@ import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { getRoom } from '@/libs/apis';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2023-10-16',
-});
+const stripe= new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2023-08-16',
+})
 
 type RequestData = {
   checkinDate: string;
