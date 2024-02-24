4ca41189c6e665bbe0e64c6dce856b79ba15c5f5
Rone47 <ryanbyrone47@gmail.com>
2024-02-24 16:35:58 +0300

    updates

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   const { data: session } = useSession();
 
   return (
-    <header className='py-20 px-6 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between'>
+    <header className='py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between'>
       <div className='flex items-center w-full md:2/3'>
         <Link href='/' className='font-black text-cyan-600'>
           Hillside Haven
